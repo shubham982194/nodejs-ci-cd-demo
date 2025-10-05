772787fed859815c5969d597d18b37d4f4e62861
Ubuntu <shubham123@jenkins.iulcft1v0ucejo5j54nftb05eh.xx.internal.cloudapp.net>
2025-10-05 10:01:43 +0000

    second

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from Node.js CI/CD demo!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
